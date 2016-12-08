c7c2d1002ef3d3ca477df2e51b407bbe0e548adc
RocketRaccoon <andreich87@gmail.com>
2016-12-08 10:57:50 +0300

    Add fail case

--- account-service-component-tests/src/test/java/com/piggymetrics/account/AccountServiceTest.java
+++ account-service-component-tests/src/test/java/com/piggymetrics/account/AccountServiceTest.java
@@ -1,14 +1,21 @@
 package com.piggymetrics.account;
 
 import com.jayway.restassured.RestAssured;
+
+import static com.jayway.restassured.filter.log.LogDetail.ALL;
+import static com.jayway.restassured.filter.log.LogDetail.BODY;
 import static org.hamcrest.core.Is.is;
 
+import com.jayway.restassured.builder.ResponseBuilder;
 import com.jayway.restassured.filter.Filter;
 import com.jayway.restassured.filter.FilterContext;
 import com.jayway.restassured.filter.log.LogDetail;
+import com.jayway.restassured.internal.RestAssuredResponseImpl;
 import com.jayway.restassured.internal.print.RequestPrinter;
 import com.jayway.restassured.internal.print.ResponsePrinter;
 import com.jayway.restassured.response.Response;
+import com.jayway.restassured.response.ResponseBody;
+import com.jayway.restassured.response.ResponseOptions;
 import com.jayway.restassured.specification.FilterableRequestSpecification;
 import com.jayway.restassured.specification.FilterableResponseSpecification;
 import com.piggymetrics.emulators.AuthServiceEmulatorClient;
@@ -19,6 +26,8 @@ import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.Attachment;
 import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.allure.annotations.Title;
+
+import java.io.PrintStream;
 
 import static com.jayway.restassured.RestAssured.given;
 import static java.net.HttpURLConnection.*;
@@ -88,7 +97,7 @@ public class AccountServiceTest {
 
         @Override
         public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
-            print(requestSpec,responseSpec, ctx);
+            print(requestSpec, responseSpec, ctx);
             return ctx.next(requestSpec, responseSpec);
         }
 
@@ -102,15 +111,28 @@ public class AccountServiceTest {
 
         @Override
         public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
-            print(requestSpec,responseSpec, ctx);
-            return ctx.next(requestSpec, responseSpec);
+            Response response = ctx.next(requestSpec, responseSpec);
+            LogDetail logDetail = LogDetail.ALL;
+            print(response, response, System.out, logDetail, true);
+            final byte[] responseBody;
+                responseBody = response.asByteArray();
+            response = cloneResponseIfNeeded(response, responseBody);
+            return response;
+        }
+
+        private Response cloneResponseIfNeeded(Response response, byte[] responseAsString) {
+            if (responseAsString != null && response instanceof RestAssuredResponseImpl && !((RestAssuredResponseImpl) response).getHasExpectations()) {
+                final Response build = new ResponseBuilder().clone(response).setBody(responseAsString).build();
+                ((RestAssuredResponseImpl) build).setHasExpectations(true);
+                return build;
+            }
+            return response;
         }
 
 
         @Attachment("Response")
-        String print(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
-            Response response = ctx.next(requestSpec, responseSpec);
-            return  ResponsePrinter.print(response, response, System.out, LogDetail.ALL, true);
+        String print(ResponseOptions responseOptions, ResponseBody responseBody, PrintStream stream, LogDetail logDetail, boolean shouldPrettyPrint) {
+            return  ResponsePrinter.print(responseOptions, responseBody, stream, logDetail, shouldPrettyPrint);
         }
     }
 }
